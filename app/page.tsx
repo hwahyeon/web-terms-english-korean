7cd755a0af496ef5b31e2e05aebcb5deb598c90b
hwahyeon <hwahyeon@knou.ac.kr>
2024-04-11 23:51:34 +0900

    feat: Add the contents of readme.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
 import styles from "./page.module.css";
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 export default function Home() {
+  const [mdData, setMdData] = useState<string>("");
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/hwahyeon/terms-english-korean/main/README.md"
+    )
+      .then((response: Response) => response.text())
+      .then((text: string) => {
+        const html: string = marked.parse(text);
+        setMdData(html);
+      })
+      .catch((error: Error) =>
+        console.error("Error fetching markdown:", error)
+      );
+  }, []);
+
   return (
     <main className={styles.main}>
-      Project
+      <div
+        className="markdown-content"
+        dangerouslySetInnerHTML={{ __html: mdData }}
+      ></div>
     </main>
   );
 }
